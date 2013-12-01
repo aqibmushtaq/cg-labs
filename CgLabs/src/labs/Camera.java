b93d9e71fd7f6c260bd9255a897e7924a5ad7f96
Aqib Mushtaq <aqib@mushtaq.me>
2013-12-01 03:14:06 +0000

    Added Camera and ParallelAnimator

---
+++ CgLabs/src/labs/Camera.java
@@ -0,0 +1,69 @@
+package labs;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Aqib
+ * Date: 30/11/13
+ * Time: 18:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class Camera {
+
+    private double xmin, xmax, ymin, ymax;
+    private double fcp, bcp;  //NOT USED: front & back clippling planes
+    private double ax, bx, ay, by;
+
+    public Camera(double xmin, double xmax, double ymin, double ymax) {
+        this.xmin = xmin;
+        this.xmax = xmax;
+        this.ymin = ymin;
+        this.ymax = ymax;
+    }
+
+    public Vector3D getVPN() {
+        Vector3D v1 = new Vector3D(1,0,0);
+        Vector3D v2 = new Vector3D(0,1,0);
+        return v1.crossProduct(v2);
+    }
+
+    private Point3D cameraTransform(final Point3D p) {
+        return new Point3D(p.x, p.y, p.z);
+    }
+
+    private Point3D projectionTransform(final Point3D p) {
+        return new Point3D(p.x,p.y,0);
+    }
+
+    private Point3D viewportTransform(final Point3D p) {
+        double xPrime = ax + bx*p.x;
+        double yPrime = ay + by*p.y;
+        return new Point3D(xPrime, yPrime, 0);
+    }
+
+    public final Point3D project(final Point3D p) {
+        Point3D temp = cameraTransform(p);
+        temp = projectionTransform(temp);
+        return viewportTransform(temp);
+    }
+
+    public void setViewport(int width, int height) {
+        double vXMin = 0;
+        double vXMax = width;
+
+        double vYMin = 0;
+        double vYMax = height;
+
+        double dVX = vXMax - vXMin;
+        double dVY = vYMax - vYMin;
+
+        double dWX = xmax - xmin;
+        double dWY = ymax - ymin;
+
+        bx = dVX / dWX;
+        by = dVY / dWY;
+
+        ax = vXMin - bx*xmin;
+        ay = vYMin - by*ymin;
+    }
+
+}
---
+++ CgLabs/src/labs/ParallelAnimator.java
@@ -0,0 +1,49 @@
+package labs;
+
+import java.awt.*;
+import javax.swing.*;
+
+import static java.lang.Math.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Aqib
+ * Date: 30/11/13
+ * Time: 18:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class ParallelAnimator extends Animator {
+    private static final String[] files = {"./cube.dat", "./pyramid.dat"};
+    protected Camera camera;
+    private Scene scene;
+
+    public ParallelAnimator() {
+        super();
+
+        scene = new Scene(files);
+        setupCamera();
+    }
+
+    protected void setupCamera() {
+        camera = new Camera(-5, 5, -5, 5);
+    }
+
+    protected void animate(Graphics g) {
+        camera.setViewport(getWidth(), getHeight());
+
+        if (g == null || scene == null || camera == null)
+            return;
+
+        Matrix mX = new Matrix(), mY = new Matrix(), mZ = new Matrix();
+        mX.setRotationX(-PI / 11);
+        mY.setRotationY(PI / 13);
+        mZ.setRotationZ(PI / 17);
+        scene.transform(mZ.multiply(mY.multiply(mX)));
+
+        scene.draw(camera, g);
+    }
+
+    public static void main(String[] args) {
+        new ParallelAnimator().loop();
+    }
+}
