8385f98058515341da70658ca298101c8e3b02a7
Aqib Mushtaq <aqib@mushtaq.me>
2013-12-01 03:12:53 +0000

    Implemented Scene class which holds objects to be shown on screen

--- CgLabs/src/labs/Scene.java
+++ CgLabs/src/labs/Scene.java
@@ -3,20 +3,59 @@ package labs;
 import java.awt.*;
 
 public class Scene {
-	private GObject[] obj;
+	private GObject[] objs;
 
-	public Scene(String[] fileName) {
+	public Scene(String[] fileNames) {
+        objs = new GObject[fileNames.length];
+        for (int i = 0; i < fileNames.length; i++)
+            objs[i] = new GObject(fileNames[i]);
 	}
 
 	public void transform(Matrix m) {
+        for (GObject o : objs)
+            o.transform(m);
 	}
 
-	// wait until next lab for Camera
-	// public void draw(Camera c, Graphics g){}
+	public void draw(Camera c, Graphics g) {
+        //get the view point vector
+        Vector3D vpn = c.getVPN();
+
+        //go through all the objects in this scene
+        for (int i = 0; i < objs.length; i++) {
+            GObject currObj = objs[i];
+
+            //convert all the points to this object to map to this view port
+            Point3D[] points = new Point3D[currObj.vertex.length];
+            for (int j = 0; j < currObj.vertex.length; j++)
+                points[j] = c.project(currObj.vertex[j]);
+
+            //draw each face of this object
+            for (int j = 0; j < currObj.face.length; j++) {
+                Face currFace = currObj.face[j];
+
+                int indexLength = currFace.index.length;
+                int[] xVals = new int[indexLength];
+                int[] yVals = new int[indexLength];
+                //check if the face is front facing
+                if (Point3D.isFrontFace(points[currFace.index[0]], points[currFace.index[1]], points[currFace.index[2]], vpn)) {
+                    //store all the points which make up the face
+                    for (int k = 0; k < indexLength; k++) {
+                        xVals[k] = (int) points[currFace.index[k]].x;
+                        yVals[k] = (int) points[currFace.index[k]].y;
+                    }
+                }
+
+                g.setColor(currFace.color);
+                g.fillPolygon(xVals, yVals, indexLength);
+            }
+
+
+        }
+    }
 
 	public String toString() {
 		String str = "";
-		for (GObject o : obj) 
+		for (GObject o : objs)
 			str += "\n" + o.toString();
 		return str;
 	}
